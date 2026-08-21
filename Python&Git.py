a5fbc7d409d8145575d47630f99acedc652dc8d7
김재민 <jamin3874@gmail.com>
2026-08-21 21:53:34 +0900

    Initial commit

---
+++ Python&Git.py
@@ -0,0 +1,6 @@
+age = input("나이가 몇살이에요?" )
+age = int(age)
+if age >=20:
+    print("성인이시네요")
+else:
+    print("아직 미성년자시군요")
---
+++ README.md
@@ -0,0 +1,3 @@
+# Python Git Manager
+
+Python과 Git 개발 환경을 확인하기 위한 실습 프로젝트입니다.
